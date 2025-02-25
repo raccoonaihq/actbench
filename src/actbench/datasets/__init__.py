e206cb959eef7a846f610089fbd268c03108d4b4
scorchy38 <shubh@flyingraccoon.tech>
2025-02-26 02:34:45 +0530

    fix: download dataset from remote if not available

--- src/actbench/datasets/__init__.py
+++ src/actbench/datasets/__init__.py
@@ -1,9 +1,29 @@
+import logging
+import os
 from typing import Dict, Any, List
+
+import requests
 
 from .base import BaseDataset
 from .json import JsonDataset
 
-_DATASET_INSTANCE: BaseDataset = JsonDataset("dataset.jsonl")
+DATASET_URL = "https://raw.githubusercontent.com/raccoonaihq/actbench/master/dataset.jsonl"
+LOCAL_DATASET_PATH = "dataset.jsonl"
+
+
+def download_dataset():
+    if not os.path.exists(LOCAL_DATASET_PATH):
+        response = requests.get(DATASET_URL)
+        if response.status_code == 200:
+            with open(LOCAL_DATASET_PATH, "w", encoding="utf-8") as file:
+                file.write(response.text)
+        else:
+            logging.error(f"Failed to download dataset: HTTP {response.status_code}")
+
+
+download_dataset()
+
+_DATASET_INSTANCE: BaseDataset = JsonDataset(LOCAL_DATASET_PATH)
 
 
 def load_task_data(task_id: str | int) -> Dict[str, Any]:
@@ -14,5 +34,5 @@ def get_all_task_ids() -> List[str]:
     return _DATASET_INSTANCE.get_all_task_ids()
 
 
-def get_all_tasks() -> List[dict[str, Any]]:
+def get_all_tasks() -> List[Dict[str, Any]]:
     return _DATASET_INSTANCE.get_all_tasks()
